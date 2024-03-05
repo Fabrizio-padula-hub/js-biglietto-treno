1d4009d71986b260893c697e319817b444f7e6d0
Fabrizio-padula-hub <fabriziopadula80@gmail.com>
2024-03-05 16:07:05 +0100

    feat: add priceTiket

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+// L'output del prezzo finale va messo fuori in forma umana
+// (con massimo due decimali, per indicare centesimi sul prezzo).
+// Questo richiederà un minimo di ricerca.
+
+
+// RACCOLTA DATI UTENTE
+// chiedere i chilometri
+const userKm = parseInt (prompt ('Scrivimi i chilometri'));
+console.log(userKm);
+
+// chiedere l'età
+const userAge = parseInt (prompt ('Scrivimi la tua età'));
+console.log(userAge);
+
+
+// CALCOLARE 
+// calcolare solo il prezzo del biglietto
+const priceTiket = parseFloat(`${userKm * 0.21}`);
+console.log(priceTiket);
+
+
+// SCONTO
+// calcolare sconto del 20% ai minorenni
+// calcolare sconto del 40% agli over 65
+
+
+// STAMPARE
+// .....
+
